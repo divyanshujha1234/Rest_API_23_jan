42940dfe6b9fbbb113e9b677524735689d3264de
divyanshu jha <divyanshu3636@gmail.com>
2025-01-30 18:22:22 +0530

    Headers and http status code

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app= express();
 
 const users= require("./MOCK_DATA.json");
 const fs= require("fs");
+const { connected } = require("process");
 
 const port= 8000;
 
@@ -12,6 +13,9 @@ app.use(express.urlencoded({extended: false})); // plugin or a middleware
 //routes
 
 app.get("/api/users", (req,res)=>{
+
+    res.setHeader("X-MyName", "Divyanshu"); //Custom header
+
    return  res.json(users);
 });
 
@@ -27,10 +31,14 @@ app.get("/api/users/:id", (req,res)=>{  //Dynamic Path Parammeters
 
 app.post("/api/users" , (req,res)=>{
     const body = req.body;
+    if(!body || !body.first_name || !body.last_name || !body.email || !body.gender || !body.JOB_TITLE)
+    {
+        return res.status(400).json({"msg": "Rikt Sthaan Bhro!"});
+    }
     users.push({...body, id: users.length +1});
     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err,data)=>{
          
-        return res.json({Status: "pending"});
+        return res.status(201).json({Status: "Success"});
     });
 
     //TODO - create the user
@@ -51,3 +59,22 @@ app.delete("/api/users/:id" , (req,res)=>{
 
 
 app.listen(port, ()=> console.log(`Server Started at port ${port}`));
+
+
+//middleware runs ov every req,res excess.
+// It can---
+// 1.Excute any connected
+// 2.Make changes to req and res objects.
+// 3.End the req-res cycle. 
+// 4.Call the next Middleware function in the stack. 
+
+//**********HEADERS***********
+//it is a Meta Data (format,to,content_type) that gives extra info about req and res.
+//Always append -X in front of custom headers. 
+
+//***********HTTP STATUS CODES***********
+// 1xx (Informational): 100 Continue, 101 Switching Protocols, 103 Early Hints.
+// 2xx (Success): 200 OK, 201 Created, 202 Accepted, 204 No Content, 206 Partial Content.
+// 3xx (Redirection): 301 Moved Permanently, 302 Found, 304 Not Modified, 307 Temporary Redirect, 308 Permanent Redirect.
+// 4xx (Client Errors): 400 Bad Request, 401 Unauthorized, 403 Forbidden, 404 Not Found, 405 Method Not Allowed, 408 Request Timeout, 409 Conflict, 410 Gone, 418 I'm a Teapot, 429 Too Many Requests.
+// 5xx (Server Errors): 500 Internal Server Error, 502 Bad Gateway, 503 Service Unavailable, 504 Gateway Timeout.
